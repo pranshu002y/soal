3eef4eae15c97a2d3db7ac5df1ac3d839a42917e
Pranshu Yadav <pranshu.yadav@venturepact.com>
2024-01-14 20:19:50 +0530

    react icons eras

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { FaRegFileAlt } from "react-icons/fa";
-import { LuDownload } from "react-icons/lu";
+
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 const Card = () => {
@@ -31,7 +30,7 @@ const Card = () => {
 {data && data.map((e)=> (
 
 <div className="relative w-60 h-72 rounded-[40px] bg0 text-white py-5 px-5 overflow-hidden z-[9] card-hover" onClick={() => navigate("/Photoview")} key={e.id}>
-        <FaRegFileAlt />
+        page
         <div className="lorem"> 
         <p className="text-sm mt-5 font-semibold leading-tight  ">
           {e.company}
@@ -42,7 +41,7 @@ const Card = () => {
           <div className="flex items-center justify-between mb-3 px-8 py-3">
             <h5>4.5MB</h5>
             <span className="w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center">
-            <LuDownload color="#fff"/>  
+            <p color="#fff"/>  
             </span>
           </div>
           <div className="tag w-full py-4 bg-green-600 flex items-center justify-center">
